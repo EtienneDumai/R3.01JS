034692b630b767e50b9fca03990185cc8641a37a
Etienne Dumai <etienne.dumai@gmail.com>
2024-09-10 14:12:40 +0200

    Avancer dans le TP ex4 q4

--- TD/TD1/main.js
+++ TD/TD1/main.js
@@ -1,1 +1,21 @@
-console.log(0.1 + 0.2); // Output: 'BrainStation'
+
+
+//La première instruction `console.log` récupère la valeur du deuxième élément enfant de l'élément ayant l'identifiant "addForm".
+console.log(document.getElementById("addForm").children[1].value);
+//La deuxième instruction `console.log` récupère la valeur du deuxième élément enfant de l'élément parent de l'élément ayant l'identifiant "item".
+console.log(document.getElementById("item").parentNode.children[1].value);
+
+/*La troisième instruction `console.log` récupère la valeur d'un élément profondément imbriqué dans la structure du DOM. Elle part 
+de l'élément ayant l'identifiant "filter" et traverse plusieurs niveaux dans l'arbre du DOM pour finalement récupérer la valeur d'un élément spécifique.*/
+console.log(document.getElementById("filter").parentNode.parentNode.parentNode.parentNode.parentNode.children[2].children[0].children[1].children[1].value);
+
+function modifPlaceHolder() {
+
+    document.getElementById("filter").placeholder = "Rechercher un item dans la table … ";
+}
+function recupClass(idcherche) {
+    console.log(document.getElementById(idcherche).parentNode.children[1].className);
+}
+function recupName(namecherche) {
+    console.log(document.getElementByName(namecher>che).type);
+}
