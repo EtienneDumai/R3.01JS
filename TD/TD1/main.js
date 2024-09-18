25ccc5a525d9ce242b5c04b68ec7d8f770e11966
Etienne Dumai <etienne.dumai@gmail.com>
2024-09-18 15:57:09 +0200

    Arrivé à la question 10

--- TD/TD1/main.js
+++ TD/TD1/main.js
@@ -13,9 +13,67 @@ function modifPlaceHolder() {
 
     document.getElementById("filter").placeholder = "Rechercher un item dans la table … ";
 }
-function recupClass(idcherche) {
+function recupClassQ2(idcherche) {
     console.log(document.getElementById(idcherche).parentNode.children[1].className);
 }
+function recupNameQ3(namecherche) {
+    console.log(document.getElementByName(namecher > che).type);
+}
+function getHeadersQ4() {
+
+    let headers = document.querySelectorAll('.table thead th'); headers.forEach(e => { console.log(e.textContent); });
+}
+
+function applyBorderStyleQ5(){
+    document.getElementByTagName("table").style.border = "1px solid black";
+}
+function applyBoderStyleByClassNameQ6(){
+    document.getElementByClassName("table").style.border = "1px solid black";
+}
+function changeValueQ7(classe) {
+    document.getElementByClassName(classe).value = "Nouvelle valeur";
+}
+function knowIdQ8() {
+    const itemElement = document.getElementById("item");
+    const cardElement = itemElement.closest(".card");
+    const cardId = cardElement.id;
+    console.log(cardId);
+}
+function recupClass(idcherche) {
+    console.log(document.querySelector("#" + idcherche + " > :nth-child(2)").className);
+}
+
 function recupName(namecherche) {
-    console.log(document.getElementByName(namecher>che).type);
+    console.log(document.querySelector("[name='" + namecherche + "']").type);
+}
+
+function getHeaders() {
+    let headers = document.querySelectorAll('.table thead th');
+    headers.forEach(e => {
+        console.log(e.textContent);
+    });
+}
+
+function applyBorderStyle() {
+    document.querySelector("table").style.border = "1px solid black";
+}
+
+function applyBoderStyleByClassName() {
+    document.querySelector(".table").style.border = "1px solid black";
+}
+
+function changeValue(classe) {
+    document.querySelectorAll("." + classe).forEach(e => {
+        e.value = "Nouvelle valeur";
+    });
+}
+
+function knowId() {
+    const itemElement = document.querySelector("#item");
+    const cardElement = itemElement.closest(".card");
+    const cardId = cardElement.id;
+    console.log(cardId);
+}
+function Q10(params) {
+    
 }
